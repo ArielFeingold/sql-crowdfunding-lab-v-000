37596f1daf3453595b09acb14be0458c89980f57
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-19 15:12:37 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-#projects
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_1", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_2", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_3", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_4", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_5", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_6", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_7", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_8", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_9", "gadgets", 1500, 3/12/2017, 9/1/2018)
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_10", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- #projects
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_1", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_2", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_3", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_4", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_5", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_6", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_7", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_8", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_9", "gadgets", 1500, 3/12/2017, 9/1/2018)
+-- INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("project_10", "gadgets", 1500, 3/12/2017, 9/1/2018)
 
-#users
+-- #users
 INSERT INTO users (name, age) VALUES ("Jon", 25)
 INSERT INTO users (name, age) VALUES ("Ben", 25)
 INSERT INTO users (name, age) VALUES ("Jeremy", 25)
